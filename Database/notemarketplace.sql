c53bae6c7b9f750c5202b00d28e7baabc02d959d
Parth-B-Parmar <77610212+Parth-B-Parmar@users.noreply.github.com>
2021-02-13 20:26:38 +0530

    Update notemarketplace.sql

--- Database/notemarketplace.sql
+++ Database/notemarketplace.sql
@@ -1,12 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 5.0.4
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Feb 13, 2021 at 03:52 PM
--- Server version: 10.4.17-MariaDB
--- PHP Version: 8.0.2
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
