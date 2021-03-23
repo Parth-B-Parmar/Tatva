08041489fa9f826c3a6d4ef56b012374b49bd37d
Parth-B-Parmar <77610212+Parth-B-Parmar@users.noreply.github.com>
2021-03-23 12:51:07 +0530

    Delete PHP directory

--- PHP/Front/js/add-notes.js
+++
@@ -1,8 +0,0 @@
-function confirmBox() {
-    
-    var confirmation = confirm("Publishing this note will send note to administrator for review, once administrator review and approve then this note will be published to portal. Press yes to continue.");
-    
-    if(confirmation == true) {
-        window.location.href = "dashboard.html";
-    }
-}
